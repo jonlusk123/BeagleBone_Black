885ebf20ea551360d65ed77ce75886c85b3704f1
Jon Lusk <jonlusk123@gmail.com>
2014-04-10 02:28:46 -0400

    emax_es08a_servo.h

---
+++ emax_es08a_servo.h
@@ -0,0 +1,8 @@
+#ifndef emax_es08a_servo_h
+#define emax_es08a_servo_h
+
+#define EMAX_ES08A_PWM_PERIOD 		(unsigned long)20000000			/* In nanoseconds */
+#define EMAX_ES08A_PWM_DUTY_MIN		(unsigned long)480000			/* In nanoseconds */
+#define EMAX_ES08A_PWM_DUTY_MAX		(unsigned long)2100000			/* In nanoseconds */
+
+#endif
